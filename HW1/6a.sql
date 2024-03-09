fd8383ec6170fc4c1308bd69a75fd957b7aaf863
MarkoHaralovic <mharalovic11@gmail.com>
2024-03-09 16:23:17 +0100

    6a sql fixed; added event_name column

--- HW1/6a.sql
+++ HW1/6a.sql
@@ -1,6 +1,6 @@
 CREATE TABLE mharalovic.daily_user_activity (
     `event_date` Date,
-    `event_name` Date,
+    `event_name` LowCardinality(String),
     `user_pseudo_id` String,
     `geo_country` LowCardinality(String),
     `platform` ENUM('ANDROID'=1,'IOS'=2,'WEB'=3),
@@ -8,84 +8,90 @@ CREATE TABLE mharalovic.daily_user_activity (
 )
 ENGINE = MergeTree
 PARTITION BY toYYYYMM(event_date)
-ORDER BY (event_date,user_pseudo_id,platform,geo_country)
+ORDER BY (event_date,user_pseudo_id,platform,geo_country,event_name)
 SETTINGS index_granularity = 8192;
 
 --january
 INSERT INTO mharalovic.daily_user_activity
 SELECT
     toDate(bq.events.event_date) as event_date,
+    bq.events.event_name,
     bq.events.user_pseudo_id,
     bq.events.geo_country,
     bq.events.platform,
     COUNT(bq.events.event_name) as event_name_count
-FROM bq.events 
+FROM bq.events
 WHERE bq.events.event_date BETWEEN '20230101' AND '20230131'
-GROUP BY event_date, bq.events.user_pseudo_id, bq.events.platform, bq.events.geo_country;
+GROUP BY bq.events.event_date,bq.events.event_name, bq.events.user_pseudo_id, bq.events.platform, bq.events.geo_country;
 
 --february
 INSERT INTO mharalovic.daily_user_activity
 SELECT
     toDate(bq.events.event_date) as event_date,
+    bq.events.event_name,
     bq.events.user_pseudo_id,
     bq.events.geo_country,
     bq.events.platform,
     COUNT(bq.events.event_name) as event_name_count
 FROM bq.events
 WHERE bq.events.event_date BETWEEN '20230201' AND '20230228'
-GROUP BY event_date, bq.events.user_pseudo_id, bq.events.platform, bq.events.geo_country;
+GROUP BY bq.events.event_date,bq.events.event_name, bq.events.user_pseudo_id, bq.events.platform, bq.events.geo_country;
 
 --march
 INSERT INTO mharalovic.daily_user_activity
 SELECT
     toDate(bq.events.event_date) as event_date,
+    bq.events.event_name,
     bq.events.user_pseudo_id,
     bq.events.geo_country,
     bq.events.platform,
     COUNT(bq.events.event_name) as event_name_count
 FROM bq.events
 WHERE bq.events.event_date BETWEEN '20230301' AND '20230331'
-GROUP BY event_date, bq.events.user_pseudo_id, bq.events.platform, bq.events.geo_country;
+GROUP BY bq.events.event_date,bq.events.event_name, bq.events.user_pseudo_id, bq.events.platform, bq.events.geo_country;
 
 --april
 INSERT INTO mharalovic.daily_user_activity
 SELECT
     toDate(bq.events.event_date) as event_date,
+    bq.events.event_name,
     bq.events.user_pseudo_id,
     bq.events.geo_country,
     bq.events.platform,
     COUNT(bq.events.event_name) as event_name_count
 FROM bq.events
 WHERE bq.events.event_date BETWEEN '20230401' AND '20230430'
-GROUP BY event_date, bq.events.user_pseudo_id, bq.events.platform, bq.events.geo_country;
+GROUP BY bq.events.event_date,bq.events.event_name, bq.events.user_pseudo_id, bq.events.platform, bq.events.geo_country;
 
 --may
 INSERT INTO mharalovic.daily_user_activity
 SELECT
     toDate(bq.events.event_date) as event_date,
+    bq.events.event_name,
     bq.events.user_pseudo_id,
     bq.events.geo_country,
     bq.events.platform,
     COUNT(bq.events.event_name) as event_name_count
 FROM bq.events
 WHERE bq.events.event_date BETWEEN '20230501' AND '20230531'
-GROUP BY event_date, bq.events.user_pseudo_id, bq.events.platform, bq.events.geo_country;
+GROUP BY bq.events.event_date,bq.events.event_name, bq.events.user_pseudo_id, bq.events.platform, bq.events.geo_country;
 
---ISSUE :[2024-03-07 16:28:58] Read timed out, server ClickHouseNode FOR MAY
 --june
 INSERT INTO mharalovic.daily_user_activity
 SELECT
     toDate(bq.events.event_date) as event_date,
+    bq.events.event_name,
     bq.events.user_pseudo_id,
     bq.events.geo_country,
     bq.events.platform,
     COUNT(bq.events.event_name) as event_name_count
 FROM bq.events
 WHERE bq.events.event_date BETWEEN '20230601' AND '20230630'
-GROUP BY event_date, bq.events.user_pseudo_id, bq.events.platform, bq.events.geo_country;
+GROUP BY bq.events.event_date,bq.events.event_name, bq.events.user_pseudo_id, bq.events.platform, bq.events.geo_country;
 
 CREATE TABLE mharalovic.monthly_user_activity (
     `start_of_month` Date,
+    `event_name` LowCardinality(String),
     `user_pseudo_id` String,
     `geo_country` LowCardinality(String),
     `platform` ENUM('ANDROID'=1,'IOS'=2,'WEB'=3),
@@ -93,13 +99,14 @@ CREATE TABLE mharalovic.monthly_user_activity (
 )
 ENGINE = MergeTree
 PARTITION BY start_of_month
-ORDER BY (start_of_month,user_pseudo_id,platform,geo_country)
+ORDER BY (start_of_month,user_pseudo_id,platform,geo_country,event_name)
 SETTINGS index_granularity = 8192;
 
 --january
 INSERT INTO mharalovic.monthly_user_activity
 SELECT
     toStartOfMonth(mharalovic.daily_user_activity.event_date) as start_of_month,
+         mharalovic.daily_user_activity.event_name,
          mharalovic.daily_user_activity.user_pseudo_id,
          mharalovic.daily_user_activity.geo_country,
          mharalovic.daily_user_activity.platform,
@@ -109,11 +116,13 @@ WHERE toStartOfMonth(mharalovic.daily_user_activity.event_date)='2023-01-01'
 group by start_of_month,
          mharalovic.daily_user_activity.user_pseudo_id,
          mharalovic.daily_user_activity.geo_country,
-         mharalovic.daily_user_activity.platform;
+         mharalovic.daily_user_activity.platform,
+         mharalovic.daily_user_activity.event_name;
 --february
 INSERT INTO mharalovic.monthly_user_activity
 SELECT
     toStartOfMonth(mharalovic.daily_user_activity.event_date) as start_of_month,
+         mharalovic.daily_user_activity.event_name,
          mharalovic.daily_user_activity.user_pseudo_id,
          mharalovic.daily_user_activity.geo_country,
          mharalovic.daily_user_activity.platform,
@@ -123,11 +132,13 @@ WHERE toStartOfMonth(mharalovic.daily_user_activity.event_date)='2023-02-01'
 group by start_of_month,
          mharalovic.daily_user_activity.user_pseudo_id,
          mharalovic.daily_user_activity.geo_country,
-         mharalovic.daily_user_activity.platform;
+         mharalovic.daily_user_activity.platform,
+         mharalovic.daily_user_activity.event_name;
 --march
 INSERT INTO mharalovic.monthly_user_activity
 SELECT
     toStartOfMonth(mharalovic.daily_user_activity.event_date) as start_of_month,
+         mharalovic.daily_user_activity.event_name,
          mharalovic.daily_user_activity.user_pseudo_id,
          mharalovic.daily_user_activity.geo_country,
          mharalovic.daily_user_activity.platform,
@@ -137,11 +148,14 @@ WHERE toStartOfMonth(mharalovic.daily_user_activity.event_date)='2023-03-01'
 group by start_of_month,
          mharalovic.daily_user_activity.user_pseudo_id,
          mharalovic.daily_user_activity.geo_country,
-         mharalovic.daily_user_activity.platform;
+         mharalovic.daily_user_activity.platform,
+         mharalovic.daily_user_activity.event_name;
+
 --april
 INSERT INTO mharalovic.monthly_user_activity
 SELECT
     toStartOfMonth(mharalovic.daily_user_activity.event_date) as start_of_month,
+         mharalovic.daily_user_activity.event_name,
          mharalovic.daily_user_activity.user_pseudo_id,
          mharalovic.daily_user_activity.geo_country,
          mharalovic.daily_user_activity.platform,
@@ -151,11 +165,14 @@ WHERE toStartOfMonth(mharalovic.daily_user_activity.event_date)='2023-04-01'
 group by start_of_month,
          mharalovic.daily_user_activity.user_pseudo_id,
          mharalovic.daily_user_activity.geo_country,
-         mharalovic.daily_user_activity.platform;
+         mharalovic.daily_user_activity.platform,
+         mharalovic.daily_user_activity.event_name;
+
 --may
 INSERT INTO mharalovic.monthly_user_activity
 SELECT
     toStartOfMonth(mharalovic.daily_user_activity.event_date) as start_of_month,
+         mharalovic.daily_user_activity.event_name,
          mharalovic.daily_user_activity.user_pseudo_id,
          mharalovic.daily_user_activity.geo_country,
          mharalovic.daily_user_activity.platform,
@@ -165,11 +182,14 @@ WHERE toStartOfMonth(mharalovic.daily_user_activity.event_date)='2023-05-01'
 group by start_of_month,
          mharalovic.daily_user_activity.user_pseudo_id,
          mharalovic.daily_user_activity.geo_country,
-         mharalovic.daily_user_activity.platform;
+         mharalovic.daily_user_activity.platform,
+         mharalovic.daily_user_activity.event_name;
+
 --june
 INSERT INTO mharalovic.monthly_user_activity
 SELECT
     toStartOfMonth(mharalovic.daily_user_activity.event_date) as start_of_month,
+         mharalovic.daily_user_activity.event_name,
          mharalovic.daily_user_activity.user_pseudo_id,
          mharalovic.daily_user_activity.geo_country,
          mharalovic.daily_user_activity.platform,
@@ -179,5 +199,5 @@ WHERE toStartOfMonth(mharalovic.daily_user_activity.event_date)='2023-06-01'
 group by start_of_month,
          mharalovic.daily_user_activity.user_pseudo_id,
          mharalovic.daily_user_activity.geo_country,
-         mharalovic.daily_user_activity.platform;
-
+         mharalovic.daily_user_activity.platform,
+         mharalovic.daily_user_activity.event_name;
