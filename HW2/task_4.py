724cf6748b1c350f4d383582c331052343dd9bbc
MarkoHaralovic <mharalovic11@gmail.com>
2024-03-15 22:10:31 +0100

    task_4 finished,needs decision on where to check the date + when to save the csvs

--- HW2/task_4.py
+++ HW2/task_4.py
@@ -14,6 +14,16 @@ def camel_case(s):
     s = re.sub(r"(_|-)+", " ", s).title().replace(" ", "")
     return ''.join([s[0].lower(), s[1:]])
  
+def get_camel_column_names():
+   column_names = ['event_date', 'event_timestamp', 'event_name', 'user_pseudo_id', 'geo_country', 
+                   'app_info_version', 'platform', 'firebase_experiments', 'id', 'item_name', 
+                   'previous_first_open_count', 'name', 'event_id', 'status' 
+                   ]
+   camel_case_columns = [camel_case(column) for column in column_names]
+   columns_to_select = ["event_date", "event_name", "user_pseudo_id", "platform", "status", "geo_country", "id"]
+   camel_case_columns_to_select = [camel_case(column) for column in columns_to_select]
+   return camel_case_columns, camel_case_columns_to_select
+
 def _parse_arguments():
    parser = argparse.ArgumentParser()
    parser.add_argument('--csv_save_path',required=True, help='Putanja gdje se file lokalno sprema')
@@ -27,57 +37,61 @@ def search_dir(directory_path):
         for root, _, files in os.walk(directory_path):
             for file in files:
                 if file.endswith('.csv'):
-                    full_path = os.path.join(root, file)
+                    full_path = os.path.join(root, file).replace('\\', '/')
                     file_paths.append(full_path)
-                    #print(full_path)
     return file_paths
-def merge_data(file_paths,csv_file_path):
-   max_file_size = 1000000000 # size in bytes, if csv larger than this, load it in chunks
-   chunk_size=100000
-   column_names = ['event_date', 'event_timestamp', 'event_name', 'user_pseudo_id', 'geo_country', 
-                   'app_info_version', 'platform', 'firebase_experiments', 'id', 'item_name', 
-                   'previous_first_open_count', 'name', 'event_id', 'status' 
-                   ]
-   camel_case_columns = [camel_case(column) for column in column_names]
-   columns_to_select = ["event_date", "event_name", "user_pseudo_id", "platform", "status", "geo_country", "id"]
+ 
+def merge_data(file_paths,csv_file_path,max_file_size = 1000000000,chunk_size=100000):
+   
+   camel_case_columns, camel_case_columns_to_select = get_camel_column_names()
+   
    processed_data = pd.DataFrame(columns=camel_case_columns)
+   processed_data.to_csv(csv_file_path,index=False)
    
    chunks = []
    for path in tqdm(file_paths,desc="Processing csvs"):
       if os.stat(path).st_size < max_file_size:
-         print(f"Checked {path}")
+         print(f" Checked {path}")
          csv_data = pd.read_csv(path,
                              header=None,
-                             names=column_names,
-                             usecols=columns_to_select,
+                             names=camel_case_columns,
+                             usecols=camel_case_columns_to_select,
                              on_bad_lines='skip',
-                             quoting = csv.QUOTE_NONE,#https://stackoverflow.com/questions/18016037/pandas-parsererror-eof-character-when-reading-multiple-csv-files-to-hdf5 
+                             #quoting = csv.QUOTE_NONE,#https://stackoverflow.com/questions/18016037/pandas-parsererror-eof-character-when-reading-multiple-csv-files-to-hdf5 
                              encoding='utf-8',
                              low_memory=False)
+
+         csv_data.drop_duplicates(keep='first',inplace=True)
+         csv_data['eventDate'] = pd.to_datetime(csv_data['eventDate'], format='%Y%m%d', errors='coerce')
+         csv_data = csv_data.loc[csv_data['eventDate'] <= '2023-03-15']
+         print(csv_data.head())
+         #csv_data.to_csv(csv_file_path,mode='a',index=False,header=False)
          chunks.append(csv_data)
-         
       else:
          try:
             df = pd.read_csv(path,
-                             names=column_names,
-                             usecols=columns_to_select,
+                             header=None,
+                             names=camel_case_columns,
+                             usecols=camel_case_columns_to_select,
                              chunksize=chunk_size,
                              on_bad_lines='skip',
-                             quoting=csv.QUOTE_NONE, #https://stackoverflow.com/questions/18016037/pandas-parsererror-eof-character-when-reading-multiple-csv-files-to-hdf5 
+                            # quoting=csv.QUOTE_NONE, #https://stackoverflow.com/questions/18016037/pandas-parsererror-eof-character-when-reading-multiple-csv-files-to-hdf5 
                              encoding='utf-8')
             for chunk in df:
+               chunk.drop_duplicates(keep='first',inplace=True)
+               chunk['eventDate'] = pd.to_datetime(csv_data['eventDate'], format='%Y%m%d', errors='coerce')
+               chunk = chunk.loc[chunk['eventDate'] <= '2023-03-15']
+               #chunk.to_csv(csv_file_path,mode='a',index=False)
                chunks.append(chunk)
          except pd.errors.ParserError as e:
             logging.exception(f'Error reading CSV file on path : {path}')
-   processed_data = pd.concat(chunks, ignore_index=True)   
-   processed_data.drop_duplicates(keep='First',inplace=True)
-   processed_data = loc[np.where(processed_data['eventDate'].values<='2023-03-15')]  
+   processed_data = pd.concat(chunks, ignore_index=True)
    processed_data.to_csv(csv_file_path,index=False)
    
 def main():
    csv_file_path,data_folder = _parse_arguments()
    file_paths = search_dir(directory_path=data_folder)
-   merge_data(file_paths,csv_file_path)
+   merge_data(file_paths[0:3],csv_file_path)
    return 
 
 if __name__ == "__main__":
